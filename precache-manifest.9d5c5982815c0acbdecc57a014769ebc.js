3da68340612fecc7801b27e1e18ca1a4b8634c77
 Katia S <katesakowicz@MacBook-Pro-4.local>
2020-05-26 14:43:47 +0200

    Updates

---
+++ precache-manifest.9d5c5982815c0acbdecc57a014769ebc.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "73f66c72f333196dd4d28a5dff9b811a",
+    "url": "/redux-shopping-cart/index.html"
+  },
+  {
+    "revision": "ac8b06f91698d9c4c753",
+    "url": "/redux-shopping-cart/static/css/main.9d71f0b2.chunk.css"
+  },
+  {
+    "revision": "5e72b9a098c3c1b224ba",
+    "url": "/redux-shopping-cart/static/js/2.baa91300.chunk.js"
+  },
+  {
+    "revision": "64bea2e190f27355a76963b8cb94bb7b",
+    "url": "/redux-shopping-cart/static/js/2.baa91300.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ac8b06f91698d9c4c753",
+    "url": "/redux-shopping-cart/static/js/main.2be0f3e7.chunk.js"
+  },
+  {
+    "revision": "44b14ac9869d3510c931",
+    "url": "/redux-shopping-cart/static/js/runtime-main.6c85e701.js"
+  },
+  {
+    "revision": "06eb1e304987dd7fc9698d802ec545ff",
+    "url": "/redux-shopping-cart/static/media/1.06eb1e30.jpg"
+  },
+  {
+    "revision": "14cda6ed4b1c45b965ee08c09bf720e0",
+    "url": "/redux-shopping-cart/static/media/clip.14cda6ed.mp4"
+  }
+]);
