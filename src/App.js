2f594b2d832de0867d58ced6c9537f3b355e05c9
 Katia S <katesakowicz@MacBook-Pro-4.local>
2020-05-26 14:57:25 +0200

    router updating

--- src/App.js
+++ src/App.js
@@ -2,14 +2,14 @@ import React from 'react';
 import './App.css';
 import Home from './components/home/Home';
 import Header from './components/header/Header';
-import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import { HashRouter, Route, Switch } from 'react-router-dom';
 import Cart from './components/cart/Cart';
 import Video from './components/videos/Video'
 
 function App() {
   return (
     <div className='App'>
-      <BrowserRouter basename={process.env.PUBLIC_URL}>
+      <HashRouter basename={process.env.PUBLIC_URL}>
         <Header />
         <Switch>
           <Route exact path='/' component={Home} />
@@ -17,7 +17,7 @@ function App() {
           <Route path='/cart' component={Cart} />
           <Route component={() => (<div>404 Not found </div>)} />
         </Switch>
-      </BrowserRouter>
+      </HashRouter>
     </div>
   );
 }
